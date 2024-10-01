4f3590c1a611d6bea6a5da93eb3f3025e72ad1fd
brian <brianmstricker@gmail.com>
2024-09-30 22:37:48 -0400

    style: initial layout, font, user message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
+import UserMessage from "./components/UserMessage";
+
 function App() {
- return <div>hello</div>;
+ return (
+  <div className="relative h-[80%] border-2 min-h-[200px] p-4 mx-2 sm:mx-6 my-4 w-full max-w-6xl border-white/20">
+   <h1 className="absolute text-[1.65rem] font-black tracking-widest -top-10 left-0">cmdtalk</h1>
+   <UserMessage />
+  </div>
+ );
 }
 
 export default App;
---
+++ client/src/components/UserMessage.tsx
@@ -0,0 +1,9 @@
+const UserMessage = () => {
+ return (
+  <div className="flex gap-1">
+   <div className="font-semibold">username: </div>
+   <div>msg</div>
+  </div>
+ );
+};
+export default UserMessage;
