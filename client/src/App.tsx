7a7b484bac7d2e6480a9131b79173f09b8d54661
brian <brianmstricker@gmail.com>
2024-10-03 14:01:38 -0400

    feat: cors/env/tanstack-query setup, auto scroll to bottom of message container on page load

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,31 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import TextInput from "./components/TextInput";
 import UserMessage from "./components/UserMessage";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
  const [containerMargin, setContainerMargin] = useState(3.5);
+ const query = useQuery({ queryKey: ["messages"], queryFn: getMessages });
+ const scrollContainer = useRef<HTMLDivElement>(null);
+ async function getMessages() {
+  const response = await fetch("http://127.0.0.1:8000/api");
+  if (!response.ok) {
+   throw new Error("Network response was not ok");
+  }
+  return response.json();
+ }
+ useEffect(() => {
+  if (!scrollContainer.current) return;
+  scrollContainer.current.scrollTop = scrollContainer.current.scrollHeight;
+ }, []);
  return (
   <div className="relative h-full max-h-[80vh] border-2 min-h-[200px] mx-2 sm:mx-6 my-4 w-full max-w-6xl border-white/20 flex flex-col">
    <h1 className="absolute text-[1.65rem] font-black tracking-widest -top-10 left-0">cmdtalk</h1>
-   <div className="flex flex-col gap-y-3 overflow-y-auto my-2 p-4 custom-scrollbar" style={{ marginBottom: `${containerMargin}rem` }}>
+   <div
+    ref={scrollContainer}
+    className="flex flex-col gap-y-3 overflow-y-auto my-2 p-4 custom-scrollbar"
+    style={{ marginBottom: `${containerMargin}rem` }}
+   >
     {Array.from({ length: 10 }).map((_, i) => (
      <UserMessage
       key={i}
--- server/main.go
+++ server/main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	app := fiber.New()
-    app.Get("/", func(c fiber.Ctx) error {
+    app.Use(cors.New(cors.Config{
+        AllowCredentials: true,
+        AllowOrigins:    []string{"http://localhost:5173"},
+    }))
+    err := godotenv.Load(".env")
+    PORT := os.Getenv("PORT")
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+    app.Get("/api", func(c fiber.Ctx) error {
         return c.SendString("Hello, World 👋!!")
     })
-    log.Fatal(app.Listen(":8000"))
+    log.Fatal(app.Listen(":" + PORT))
 }
