86cde0a39163b9510361d1c660b51284c5a36ab0
brian <brianmstricker@gmail.com>
2024-10-03 22:52:10 -0400

    chore: switched to fiber v2 (v3 buggy)

--- server/go.mod
+++ server/go.mod
@@ -3,16 +3,26 @@ module github.com/brianmstricker/cmdtalk
 go 1.23.1
 
 require (
+	github.com/gofiber/contrib/websocket v1.3.2
+	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/gofiber/fiber/v3 v3.0.0-beta.3 // indirect
-	github.com/gofiber/utils/v2 v2.0.0-beta.6 // indirect
+	github.com/fasthttp/websocket v1.5.8 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.17.10 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/savsgio/gotils v0.0.0-20240303185622-093b76447511 // indirect
+	github.com/tinylib/msgp v1.1.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.56.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
+	"time"
 
-	"github.com/gofiber/fiber/v3"
-	"github.com/gofiber/fiber/v3/middleware/cors"
+	"github.com/gofiber/contrib/websocket"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -13,14 +17,28 @@ func main() {
 	app := fiber.New()
     app.Use(cors.New(cors.Config{
         AllowCredentials: true,
-        AllowOrigins:    []string{"http://localhost:5173"},
+        AllowOrigins:    "http://localhost:5173",
     }))
+    app.Use(cache.New(cache.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return strings.Contains(c.Route().Path, "/ws")
+		},
+        CacheControl: true,
+        Expiration:   10 * time.Minute,
+}))
+    app.Use("/ws", func(c *fiber.Ctx) error {
+        if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+		return fiber.ErrUpgradeRequired
+	})
     err := godotenv.Load(".env")
-    PORT := os.Getenv("PORT")
     if err != nil {
         log.Fatal("Error loading .env file")
     }
-    app.Get("/api", func(c fiber.Ctx) error {
+    PORT := os.Getenv("PORT")
+    app.Get("/api", func(c *fiber.Ctx) error {
         return c.SendString("Hello, World 👋!!")
     })
     log.Fatal(app.Listen(":" + PORT))
